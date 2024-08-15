f7c89ff096a47925047bc190c9b58f91ad205693
Diego Vieira <sr.diegovieira@gmail.com>
2024-08-14 23:29:15 -0300

    Exemplos de caixa eletronico e notas escolares

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,20 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        // int nota = 6;
+
+        // if(nota >= 7)
+        //     System.out.println("Aprovado");
+        // else if (nota >= 5 && nota < 7) 
+        //     System.out.println("Prova Recuperação");
+        // else
+        //     System.out.println("Reprovado");
+
+        double alturaJoao = 1.46;
+        double alturaMarcos = 1.37;
+
+       if(alturaMarcos > alturaJoao)
+        System.out.println("João é mais alto que Marcos");
+       else
+        System.out.println("Marcos é mais baixo que João");
+    }
+}
